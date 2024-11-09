8a7fbdbb1a2a58065e30f32259414dd4eded77e7
freeman7728 <1357880399@qq.com>
2024-11-09 13:38:45 +0800

    common的query_decorator

--- internal/order/service/application.go
+++ internal/order/service/application.go
@@ -2,11 +2,10 @@ package service
 
 import (
 	"context"
-	"github.com/freeman7728/gorder-v2/order/adapters"
 	"github.com/freeman7728/gorder-v2/order/app"
 )
 
 func NewApplication(ctx context.Context) app.Application {
-	orderRepo := adapters.NewMemoryOrderRepository()
+	//orderRepo := adapters.NewMemoryOrderRepository()
 	return app.Application{}
 }
