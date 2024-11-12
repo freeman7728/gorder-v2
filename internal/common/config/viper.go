3fabf04bc445eef408cc99e83f3b0be64d214de4
freeman7728 <1357880399@qq.com>
2024-11-12 18:14:26 +0800

    stripe配置,lesson13

--- internal/common/config/viper.go
+++ internal/common/config/viper.go
@@ -1,11 +1,16 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"strings"
+)
 
 func NewViperConfig() error {
 	viper.SetConfigName("global")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("../common/config")
+	viper.EnvKeyReplacer(strings.NewReplacer("_", "-"))
 	viper.AutomaticEnv()
+	_ = viper.BindEnv("stripe-key", "STRIPE_KEY")
 	return viper.ReadInConfig()
 }
